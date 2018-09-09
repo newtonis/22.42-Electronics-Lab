ecaa309238ef899b5745993f8b116f2e210ebfad
mmaspero <mmaspero@itba.edu.ar>
2018-09-09 20:24:09 -0300

    comienzo informe ej 2

--- TP2/Cuentas y graficos/Ejercicio2.m
+++ TP2/Cuentas y graficos/Ejercicio2.m
@@ -1,13 +1,16 @@
 syms s;
-R=120;
+%R=120;
+R=632.4555;
 C=10e-9;
 L=1000e-6;
 H= 1/(1+s*C*R + (s^2) * L * C);
 step=H/s;
-K=ilaplace(step);
+K=ilaplace(step)
 
 w0=1/((L*C)^(1/2));
 p =  [L * C, C*R, 1];
+ro=roots(p)
+
 
 alpha=R/(2*L);
 sita = alpha / w0;
@@ -24,4 +27,4 @@ Habs= abs(1/(1+1i*wpico*C*R + ((1i*wpico)^2) * L * C));
 Habs
 
 
-newR=((L*C)^(1/2))/(0.2*C);
+
