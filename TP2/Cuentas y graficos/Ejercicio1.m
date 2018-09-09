a3331b37b5063e4f9f1d0e1eb39bd4a49e27648f
jmestanza <jmestanza@itba.edu.ar>
2018-09-09 12:38:46 -0300

    se acercaa

--- TP2/Cuentas y graficos/Ejercicio1.m
+++ TP2/Cuentas y graficos/Ejercicio1.m
@@ -5,15 +5,15 @@ close all;
 k=1e3;
 m=1e-3;
 C=10e-12; %el chabon de labo dijo que tiraramos entre 10 y 100 pf
-%R=0.8;
-R=3e3;
-%L=0.0097;
-L=0.001;
+R=354.6;
+L=0.956e-3;
 f=1:10:1e7;
 w=2*pi.*f;
-% Cuenta que hice a manopla , matlab me cago
+
+%Cuentas a manopla
 RE= (R./((C.*w).^2))./ (R^2 + (L.*w - 1./(C.*w)).^2);
 IMAG= ((-R^2./(C.*w)) -(L.*w - (1./(C.*w))*(L/C)))./ (R^2 + (L.*w - 1./(C.*w)).^2);
+
 filename = 'Ejercicio1.xlsx';
 xlsread(filename);
 sheet = 1;
