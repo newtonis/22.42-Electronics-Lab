e0cd84c96ba3e12ca25444f28942a6ccb65da51f
newtonis <newtonis.penguin@gmail.com>
2018-09-09 19:48:56 -0700

    Add

--- TP2/software/test02/test.py
+++ TP2/software/test02/test.py
@@ -3,6 +3,13 @@ from kivy.lang import Builder
 from kivy.uix.boxlayout import *
 from kivy.properties import *
 from kivy.uix.label import *
+from kivy.uix.button import *
+from kivy.uix.listview import *
+from kivy.uix.anchorlayout import *
+from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.uix.listview import ListView
+
+
 from math import *
 
 import numpy as np
@@ -93,7 +100,7 @@ class PointsMenu(SelectionItemA):
 
 class EndFreqMenu(SelectionItemA):
     def __init__(self, **kwargs):
-        super(EndFreqMenu,self).__init__(**kwargs)
+        super(EndFreqMenu, self).__init__(**kwargs)
         self.set_text("End freq.")
         self.update_action()
 
@@ -113,16 +120,102 @@ class StartFreqMenu(SelectionItemA):
         self.set_value_text(convert_freq(value))
 
 
-class ContainerBox(BoxLayout):
+class StartButton(Button):
+    screenmanager = ObjectProperty()
+    press_action = None
+
+    def set_press_action(self, action):
+        self.press_action = action
+
+    def release_action(self):
+        if self.press_action:
+            self.press_action()
+
+
+class MyItemButton(ListItemButton):
+    pass
+
+
+class SelectProperty(BoxLayout):
+    title_text = StringProperty("Default value")
+    items = ListProperty()
+
+    def set_title(self, text):
+        self.title_text = text
+
+
+class SelectOsc(SelectProperty):
+
+    def __init__(self, **kwargs):
+        super(SelectOsc, self).__init__(**kwargs)
+        self.set_title("Osc")
+        self.items = ["osc 1", "osc 2"]
+
+
+class SelectGen(SelectProperty):
+    def __init__(self, **kwargs):
+        super(SelectGen,self).__init__(**kwargs)
+        self.set_title("Gen")
+        self.items = ["gen 1", "gen 2", "gen 3"]
+
+
+class ContinueMenu(BoxLayout):
+    start_action = None
+    back_action = None
+
+    def start(self):
+        if self.start_action:
+            self.start_action()
+
+    def back(self):
+        if self.back_action:
+            self.back_action()
+
+
+class ContainerBox(BoxLayout, Screen):
     def __init__(self, **kwargs):
         super(ContainerBox, self).__init__(**kwargs)
+        self.ids.StartButton.set_press_action(self.go_next_menu)
+
+    def go_next_menu(self):
+        self.manager.current = 'screen2'
+
+
+class BodeMenu(BoxLayout, Screen):
+    def __init__(self, **kwargs):
+        super(BodeMenu, self).__init__(**kwargs)
+        self.ids.ContinueMenu.start_action = self.start
+        self.ids.ContinueMenu.back_action = self.back
+
+    def on_enter(self, *args):
+        pass
+
+    def start(self):
+        self.manager.current = 'screen3'
+
+    def back(self):
+        self.manager.current = 'screen1'
+
+
+class MeasMenu(BoxLayout, Screen):
+    def __init__(self, **kwargs):
+        super(MeasMenu, self).__init__(**kwargs)
 
 
 class MainApp(App):
-    container_box = ContainerBox()
 
     def build(self):
-        return self.container_box
+        my_screenmanager = ScreenManager()
+
+        container_box = ContainerBox(name='screen1')
+        bode_menu = BodeMenu(name='screen2')
+        meas_menu = MeasMenu(name='screen3')
+
+        my_screenmanager.add_widget(container_box)
+        my_screenmanager.add_widget(bode_menu)
+        my_screenmanager.add_widget(meas_menu)
+
+        return my_screenmanager
 
 
 if __name__ == '__main__':
