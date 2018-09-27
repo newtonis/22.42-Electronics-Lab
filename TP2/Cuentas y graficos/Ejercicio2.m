633b698a7a376b5615b50b6bc6d9545c8aa6898e
mmaspero <mmaspero@itba.edu.ar>
2018-09-27 18:14:01 -0300

    informe ej 2

--- TP2/Cuentas y graficos/Ejercicio2.m
+++ TP2/Cuentas y graficos/Ejercicio2.m
@@ -1,30 +1,40 @@
-syms s;
-%R=120;
-R=632.4555;
+
+%syms s;
+R=288;
+%R=632.4555;
 C=10e-9;
 L=1000e-6;
-H= 1/(1+s*C*R + (s**2) * L * C);
-step=H/s;
-K=ilaplace(step)
-
-w0=1/((L*C)**(1/2));
-p =  [L * C, C*R, 1];
-ro=roots(p)
+s = tf ('s');
+H = 1/(1+s*C*R + (s^2) * L * C);
+%step=H/s;
+%K=ilaplace(step);
 
 
-alpha=R/(2*L);
-sita = alpha / w0;
-wpico = w0*((1-2*((sita)**2))**(1/2));
-Q= 1/ (2*sita);
+% w0=1/((L*C)^(1/2));
+% p =  [L * C, C*R, 1];
+% ro=roots(p);
+% 
+% 
+% alpha=R/(2*L);
+% sita = alpha / w0;
+% wpico = w0*((1-2*((sita)^2))^(1/2));
+% Q= 1/ (2*sita);
+% 
+% x =roots (p);
+% Habs= abs(1/(1+1i*wpico*C*R + ((1i*wpico)^2) * L * C));
+% value =  20 * log10(0.2);
+% 
+% 
+% wpico = w0;
+% Habs= abs(1/(1+1i*wpico*C*R + ((1i*wpico)^2) * L * C));
+opt = stepDataOptions('InputOffset',-0.25,'StepAmplitude',0.5);
 
-x =roots (p);
-Habs= abs(1/(1+1i*wpico*C*R + ((1i*wpico)**2) * L * C));
-value =  20 * log10(0.2);
 
-Habs
-wpico = w0;
-Habs= abs(1/(1+1i*wpico*C*R + ((1i*wpico)**2) * L * C));
-Habs
+plot (second1, Volt1)
+hold on
+step(H,opt,'r')
+
+
 
 
 
