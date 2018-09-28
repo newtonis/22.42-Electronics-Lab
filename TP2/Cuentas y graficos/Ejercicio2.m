0c899fdc8d8e217d64e865c0a7b14edbd2554008
mmaspero <mmaspero@itba.edu.ar>
2018-09-27 21:26:38 -0300

    ej 4

--- TP2/Cuentas y graficos/Ejercicio2.m
+++ TP2/Cuentas y graficos/Ejercicio2.m
@@ -1,20 +1,18 @@
 
 %syms s;
-R=288;
+R=120;
 %R=632.4555;
 C=10e-9;
 L=1000e-6;
 s = tf ('s');
-H = 1/(1+s*C*R + (s^2) * L * C);
+%H = 1/(1+s*C*R + (s^2) * L * C);
+H= (s*C*R)/(s^2*L*C+s*R*C+1);
+
 %step=H/s;
 %K=ilaplace(step);
-
-
 % w0=1/((L*C)^(1/2));
 % p =  [L * C, C*R, 1];
-% ro=roots(p);
-% 
-% 
+% ro=roots(p); 
 % alpha=R/(2*L);
 % sita = alpha / w0;
 % wpico = w0*((1-2*((sita)^2))^(1/2));
@@ -27,12 +25,14 @@ H = 1/(1+s*C*R + (s^2) * L * C);
 % 
 % wpico = w0;
 % Habs= abs(1/(1+1i*wpico*C*R + ((1i*wpico)^2) * L * C));
-opt = stepDataOptions('InputOffset',-0.25,'StepAmplitude',0.5);
+%opt = stepDataOptions('InputOffset',-0.25,'StepAmplitude',0.5);
 
 
-plot (second1, Volt1)
-hold on
-step(H,opt,'r')
+ plot (second1, Volt1)
+ hold on
+%step(H,opt,'r')
+step (H)
+
 
 
 
